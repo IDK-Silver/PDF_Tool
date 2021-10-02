a717f498374cd3c48919f2cf25ce1c5b48df16cf
IDK Silver <60951976+IDK-Silver@users.noreply.github.com>
2021-10-02 19:44:06 +0800

    Remove Build Folder

--- cmake-build-debug/CMakeFiles/PDF_Tool_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PDF_Tool_autogen"
-  "PDF_Tool_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PDF_Tool_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/CMakeFiles/PDF_Tool_autogen.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for PDF_Tool_autogen.
-# This may be replaced when dependencies are built.
