e143c46415446abb2c0266f8c50d3ff1f33be386
IDK-Silver <a288235403@gmail.com>
2022-01-10 16:28:51 +0800

    新增PDF Utils Library來處理PDF，新增 PDF View Widget

---
+++ libraries/PDFUtils/PDFUtils.cpp
@@ -0,0 +1,5 @@
+//
+// Created by idk on 2022/1/7.
+//
+
+#include "PDFUtils.h"
---
+++ libraries/PDFUtils/PDFUtils.h
@@ -0,0 +1,14 @@
+//
+// Created by idk on 2022/1/7.
+//
+
+#ifndef PDF_TOOL_PDFUTILS_H
+#define PDF_TOOL_PDFUTILS_H
+
+
+class PDFUtils {
+
+};
+
+
+#endif //PDF_TOOL_PDFUTILS_H
